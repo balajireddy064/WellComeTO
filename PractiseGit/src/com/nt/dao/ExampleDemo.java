5658a00d45aebd4a5c151b0504b9a50607ef6c92
Balaji <balajireddy064@gmail.com>
2019-07-12 00:30:45 +0530

    commit 2

--- PractiseGit/src/com/nt/dao/ExampleDemo.java
+++ PractiseGit/src/com/nt/dao/ExampleDemo.java
@@ -3,6 +3,8 @@ package com.nt.dao;
 public class ExampleDemo {
 	public static void main(String[] args) {
 		System.out.println("wel come to");
+		
+		System.out.println("hello again the result comes");
 	}
 
 }
